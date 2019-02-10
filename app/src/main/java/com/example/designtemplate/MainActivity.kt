84a8b3ca1d8157c20c84565e95a32bb64caf9846
rwadada <wadada.knct@gmail.com>
2019-02-10 17:09:35 +0900

    update design
    description

--- app/src/main/java/com/example/designtemplate/MainActivity.kt
+++ app/src/main/java/com/example/designtemplate/MainActivity.kt
@@ -3,10 +3,11 @@ package com.example.designtemplate
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: com.example.designtemplate.databinding.ActivityMainBinding
+    private lateinit var binding: com.example.designtemplate.databinding.ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +20,12 @@ class MainActivity : AppCompatActivity() {
         binding.content = this
 
         binding.title.setTitle(R.string.default_title)
+
+        window.decorView.apply {
+            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
+
+        binding.title.elevation = 8f
     }
 }
 
